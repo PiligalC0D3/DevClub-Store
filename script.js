19244f8a441b582e0909a2d3a9a53c2db8acfbfa
PiligalC0dD3 <michelpiligal@gmail.com>
2025-05-04 00:50:48 -0300

    Funcionamento ok sem efeitos de transição

--- script.js
+++ script.js
@@ -10,10 +10,53 @@ const total = items.length
 let timer;
 
 
+
+
+function update (direction) {
+    
+    document.querySelector(".item.active").classList.remove("active")
+    document.querySelector(".dot.active").classList.remove("active")
+
+    if (direction > 0) {
+        active = active + 1 
+
+    
+    if (active === total) {
+        active = 0
+
+    }
+}
+
+    else if (direction < 0) {
+        active = active - 1
+
+         
+    if (active < 0) {
+        active = total - 1
+        
+    }   
+
+} 
+
+    items[active].classList.add("active")
+    dots[active].classList.add("active")
+    numberIndicator.innerHTML = `${active + 1} / ${total}`
+    list.scrollTo({
+        left: items[active].offsetLeft - 100,
+        behavior: "smooth"
+    })
+   
+    
+}
+
+
+
 prevButton.addEventListener("click",  () =>{
-    alert("Prev")
+    update(-1)
 })
 
 nextButton.addEventListener("click",  () =>{
-    alert("next")
+    update(1)
 })
+
+
